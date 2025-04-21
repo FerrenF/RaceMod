1ee1efa50f9a7f2b89d93fc41aecdf1714b98c81
Hunter W. <kfateweaver@gmail.com>
2025-04-21 19:23:53 -0400

    Delete necesse/gfx/forms/components/componentPresets directory
    Unrelated/unused package

--- necesse/gfx/forms/components/componentPresets/FormNewPlayerPreset$DrawButtonFunction.java
+++
@@ -1,7 +0,0 @@
-package necesse.gfx.forms.components.componentPresets;
-
-import necesse.gfx.forms.components.FormContentVarToggleButton;
-
-public interface FormNewPlayerPreset$DrawButtonFunction {
-	void draw(FormContentVarToggleButton var1, int var2, int var3, int var4, int var5);
-}
